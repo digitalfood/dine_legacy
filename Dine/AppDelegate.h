98fe9c437da716a03cac6a88e2cb0cf5e52581d5
pinyaoting <pinyaoting@gmail.com>
2015-02-12 21:31:42 -0800

    initial commit

---
+++ Dine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dine
+//
+//  Created by Pythis Ting on 2/12/15.
+//  Copyright (c) 2015 Yahoo!, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
