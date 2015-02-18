fe8b922afe661eb37fef24eb70582efdd7e252e0
Matt Ho <mattho@yahoo-inc.com>
2015-02-17 20:25:37 -0800

    Add Parse Framework and Facebook login

--- Dine/AppDelegate.m
+++ Dine/AppDelegate.m
@@ -6,10 +6,13 @@
 //  Copyright (c) 2015 Yahoo!, inc. All rights reserved.
 //
 
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+#import "Parse/Parse.h"
+#import <ParseUI/ParseUI.h>
 #import "AppDelegate.h"
 #import "MainViewController.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () <PFLogInViewControllerDelegate>
 
 @end
 
@@ -17,16 +20,32 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // hide status bar
-    [[UIApplication sharedApplication] setStatusBarHidden:YES];
-    // set navigation bar appearance
-    [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
-    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
-    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:121.0/255.0 green:174.0/255.0 blue:1.0 alpha:1.0]];
+    // Initiate Parse
+    [Parse setApplicationId:@"fBRzEFzV9FSrQlnUpokfbdEl4a8T6zpCMtCl5UFw" clientKey:@"jgjb5IDkcNrDJQTeQ97KyQZ3s49frMry5unqwUuu"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:nil];
+    [PFFacebookUtils initializeFacebook];
     
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    MainViewController *vc = [[MainViewController alloc] init];
-    self.window.rootViewController = vc;
+    PFUser *currentUser = [PFUser currentUser];
+    if (currentUser) {
+        // hide status bar
+        [[UIApplication sharedApplication] setStatusBarHidden:YES];
+        // set navigation bar appearance
+        [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+        [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+        [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:121.0/255.0 green:174.0/255.0 blue:1.0 alpha:1.0]];
+        
+        self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+        MainViewController *vc = [[MainViewController alloc] init];
+        self.window.rootViewController = vc;
+        
+    } else {
+        PFLogInViewController *logInController = [[PFLogInViewController alloc] init];
+        logInController.delegate = self;
+        logInController.fields = (PFLogInFieldsFacebook);
+        self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+        self.window.rootViewController = logInController;
+        [[UINavigationBar appearance] setHidden:YES];
+    }
     
     [self.window makeKeyAndVisible];
     return YES;
@@ -47,11 +66,34 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication withSession:[PFFacebookUtils session]];
+}
+
+- (void)logInViewController:(PFLogInViewController *)controller
+               didLogInUser:(PFUser *)user {
+    // hide status bar
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
+    // set navigation bar appearance
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:121.0/255.0 green:174.0/255.0 blue:1.0 alpha:1.0]];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    MainViewController *vc = [[MainViewController alloc] init];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+}
+
+- (void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController {
 }
 
 @end
